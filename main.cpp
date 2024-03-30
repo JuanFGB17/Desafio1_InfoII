5392f2d46e7eae6648059f9fa3349bfdb95e07de
JuanFGB17 <juanfgarciab17@gmail.com>
2024-03-30 16:58:34 -0500

    Llave 01

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main()
 {
-    int size, k=0;
+    int size, k=0, minsizemat;
     cout << "La llave k esta conformada por (fila, columna, valores comparativos)" << endl;
     do{
-        cout << "Cuantos valores comparativos desea ingresar" << endl;
-        cin >> size;
+        while (true){
+            cout << "Cuantos valores comparativos desea ingresar" << endl;
+            cin >> size;
+            if (cin.fail()){
+                cin.clear();
+                while (cin.get() != '\n') {
+                    continue;}}
+            else
+                break;}
     }while(size<=0);
     int *llave = new int[size + 2];
     do{
@@ -22,24 +29,46 @@ int main()
             else
                 break;}
     }while(k<0);
+    minsizemat=k;
     llave[0] = k;
     do{
-        cout << "Ingrese una columna mayor o igual a 0" << endl;
-        cin >> k;
+        while (true){
+            cout << "Ingrese una columna mayor o igual a 0" << endl;
+            cin >> k;
+            if (cin.fail()){
+                cin.clear();
+                while (cin.get() != '\n') {
+                    continue;}}
+            else
+                break;}
     }while(k<0);
+    if (minsizemat<k)
+        minsizemat=k;
+    if (minsizemat%2==0)
+        minsizemat++;
+    else
+        minsizemat += 2;
     llave[1] = k;
     cout << "Ingrese los valores comparativos (-1, 0, 1)" << endl;
     for (int i = 2; i<=size+1; i++){
         do{
-            cin >> k;
+            while (true){
+                cout << "Valor comparativo" << endl;
+                cin >> k;
+                if (cin.fail()){
+                    cin.clear();
+                    while (cin.get() != '\n') {
+                        continue;}}
+                else
+                    break;}
         }while(k!=-1 && k!=1 && k!=0);
         llave[i] = k;
     }
     for (int i = 0; i<=size+1; i++){
         cout << llave[i] << ' ';
     }
-
     delete [] llave;
-
+    cout << endl << "Minsizemat: " << minsizemat <<endl;
+    cout << "Comparaciones: " << size <<endl;
     return 0;
 }
