907a303ba2e62680d955fa3bc8d4631f9e023208
Ethanpr05 <ethan.parra@udea.edu.co>
2024-04-03 23:00:12 -0500

    Added
    Mejora de "interfaz", arreglo de errores, corrección condicionales.

--- lockGen.h
+++ lockGen.h
@@ -1,6 +1,6 @@
 #ifndef LOCKGEN_H
 #define LOCKGEN_H
 
-int *lockFinder(int **matriz, int *llave, int minSizeMat, int compare);
+int *lockFinder(int **matriz, int *llave, int minSizeMat, int compare, bool &found);
 
 #endif // LOCKGEN_H
